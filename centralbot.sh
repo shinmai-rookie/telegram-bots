5e5915fbcf43626a1c73bfe4558e9fa8d57b3794
Jaime Mosquera <shinmai_rookie@outlook.com>
2016-07-19 23:13:56 +0200

    Added comments, added variables for hard-coded strings and added support for more than one bot

--- centralbot.sh
+++ centralbot.sh
@@ -15,32 +15,55 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-. json.sh
+BIN_DIR="$(dirname "$0")"
 
-BOTKEY_FILE="$(dirname "$0")/.botkey" # This file should have the ID of your bot
-BOTKEY="$(cat "$BOTKEY_FILE")"        # Optionally, set it here as a variable
-LAST_OFFSET=0
+. "$BIN_DIR/json.sh"
+
+BOTS=("$BIN_DIR/rousbot.sh")        # Bots to run (should be runnable programs)
+BOTKEY_FILE="$BIN_DIR/.botkey"      # This file should have the ID of your bot
+BOTKEY="$(cat "$BOTKEY_FILE")"      # Optionally, set it here as a variable
+
+# Number following the update_id of the last message
+# It's a kind of lower limit for the update_id of the following message
+# It also removes the previous messages
+NEXT_OFFSET=0
+
+# Path of the shell script that defines the JSON variables
+MESSAGE_SH="$BIN_DIR/.message.sh"
+# Path of the shell script that unsets the JSON variables
+MESSAGE_UNSET_SH="$BIN_DIR/.message_unset.sh"
 
 while true; do
-    MESSAGE="$(curl --silent "https://api.telegram.org/bot$BOTKEY/getUpdates" --data 'limit=1' --data 'offset='"$LAST_OFFSET")"
+    MESSAGE="$(curl --silent "https://api.telegram.org/bot$BOTKEY/getUpdates" --data 'limit=1' --data 'offset='"$NEXT_OFFSET")"
+    # Number of lines of the file $MESSAGE_SH
     MESSAGE_SH_LINES=0
 
-    json_decode "$MESSAGE" ".message.sh"
-    sed 's/^\([^=]*\)=.*$/unset \1/' < ".message.sh" > ".message_unset.sh" &
-    . .message.sh
+    # Create both shell scripts and load the one with the assignments
+    json_decode "$MESSAGE" "$MESSAGE_SH"
+    sed 's/^\([^=]*\)=.*$/unset \1/' < "$MESSAGE_SH" > "$MESSAGE_UNSET_SH" &
+    . "$MESSAGE_SH"
 
-    LAST_OFFSET="$(expr $json_result_1_update_id + 1)"
-    MESSAGE_SH_LINES="$(wc -l < ".message.sh")"
+    # Define two variables: the lower limit for the offset, and the number of
+    # lines of the scripts
+    NEXT_OFFSET="$(expr $json_result_1_update_id + 1)"
+    MESSAGE_SH_LINES="$(wc -l < "$MESSAGE_SH")"
 
+    # If the message is not empty, run the bots
     if [ $MESSAGE_SH_LINES -gt 1 ]; then
-        bash rousbot.sh
+        for i in $(seq 0 `expr ${#BOTS[@]} - 1`); do
+            ./"${BOTS[$i]}"
+        done
     fi
 
-    while [ ! -f ".message_unset.sh" ]; do :; done
-    . .message_unset.sh
-    rm --force ".message.sh"
-    rm --force ".message_unset.sh"
+    # Wait until the unset script has been written and load it
+    while [ ! -f "$MESSAGE_UNSET_SH" ]; do :; done
+    . "$MESSAGE_UNSET_SH"
 
+    # Delete both scripts
+    rm --force "$MESSAGE_SH"
+    rm --force "$MESSAGE_UNSET_SH"
+
+    # If the last message was empty (didn't have any content besides `ok=true')
     if [ $MESSAGE_SH_LINES -le 1 ]; then
         sleep 1s
     fi
