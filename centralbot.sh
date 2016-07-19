6e4daf4f55734f38e4e312dc5d43a66dd80bcdde
Jaime Mosquera <shinmai_rookie@outlook.com>
2016-07-19 18:25:35 +0200

    Removed the JSON code from the central bot

--- centralbot.sh
+++ centralbot.sh
@@ -1,71 +1,44 @@
-LAST_OFFSET=0
+#!/bin/bash
+# json: Parse JSON files through (partly) a sed script
+# Copyright (C) 2016  Jaime Mosquera
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+. json.sh
+
 BOTKEY_FILE="$(dirname "$0")/.botkey" # This file should have the ID of your bot
 BOTKEY="$(cat "$BOTKEY_FILE")"        # Optionally, set it here as a variable
+LAST_OFFSET=0
 
-MESSAGE="$(curl --silent "https://api.telegram.org/bot$BOTKEY/getUpdates" --data 'limit=1' --data 'offset='"$LAST_OFFSET")"
+while true; do
+    MESSAGE="$(curl --silent "https://api.telegram.org/bot$BOTKEY/getUpdates" --data 'limit=1' --data 'offset='"$LAST_OFFSET")"
 
-FINAL_FILE=""
+    echo "$MESSAGE" >> OUTPUT
 
-PREFIXES=( "" )
-PREFIX_N=-1
-PREFIX=""
+    json_decode "$MESSAGE" ".message.sh"
+    sed 's/^\([^=]*\)=.*$/unset \1/' < ".message.sh" > ".message_unset.sh" &
+    . .message.sh
 
-ARRAY_NAME=( )
+    LAST_OFFSET="$(expr $json_result_1_update_id + 1)"
 
-ARRAY_COUNT=( )
-COUNT_N=-1
+    bash rousbot.sh
 
-# This doesn't work on Bash < 4.2
-# It makes the while loop not to be run in a different subshell
-# Otherwise, FINAL_FILE wouldn't be preserved outside the loop
-shopt -s lastpipe
+    while [ ! -f ".message_unset.sh" ]; do :; done
+    . .message_unset.sh
+    #rm --force ".message.sh"
 
-sed -f json.sed <<< "$MESSAGE" |
-while read LINE; do
-    if [ -z "${LINE##*=}" ]; then
-        ARRAY_NAME[$(expr $COUNT_N + 1)]="${LINE%%=}"
-
-    elif [[ "$LINE" == *"="* ]]; then
-        VAR="${LINE%%=*}"
-        VAR="$(sed 's/-/_/g' <<< "${VAR}")"
-        DEF="${LINE#*=}"
-        NEW_LINE="$PREFIX$VAR=$DEF"
-        FINAL_FILE="$FINAL_FILE$NEW_LINE"$'\n'
-
-    elif [[ "$LINE" == "START \""*"\"" ]]; then
-        NEW_PREFIX=${LINE##"START \""}
-        NEW_PREFIX=${NEW_PREFIX%%"\""}
-        PREFIX_N=$(expr $PREFIX_N + 1)
-        PREFIXES[$PREFIX_N]="$NEW_PREFIX"
-        PREFIX="$PREFIX${NEW_PREFIX}_"
-
-    elif [ "$LINE" = "START_ARRAY" ]; then
-        COUNT_N=$(expr $COUNT_N + 1)
-        ARRAY_COUNT[$COUNT_N]=0
-        PREFIX_N=$(expr $PREFIX_N + 1)
-        PREFIXES[$PREFIX_N]="$ARRAY_NAME"
-        PREFIX="$PREFIX${ARRAY_NAME}_"
-
-    elif [ "$LINE" = "END_ARRAY" ]; then
-        CURRENT_PREFIX="${PREFIXES[$PREFIX_N]}"
-        CURRENT_PREFIX="${CURRENT_PREFIX:1}"
-        ARRAY_COUNT[$COUNT_N]=0
-        ARRAY_NAME[$COUNT_N]=""
-        PREFIX="${PREFIX%%${CURRENT_PREFIX}_}"
-        COUNT_N=$(expr $COUNT_N - 1)
-        PREFIXES[$PREFIX_N]=""
-        PREFIX_N=$(expr $PREFIX_N - 1)
-
-    elif [ "$LINE" = "START" ]; then
-        PREFIX="${PREFIX%%${ARRAY_COUNT[$COUNT_N]}_}"
-        ARRAY_COUNT[$COUNT_N]=$(expr ${ARRAY_COUNT[$COUNT_N]} + 1)
-        PREFIX="$PREFIX${ARRAY_COUNT[$COUNT_N]}_"
-
-    elif [ "$LINE" = "LESS" ]; then
-        PREFIX="${PREFIX%%${PREFIXES[$PREFIX_N]}_}"
-        PREFIXES[$PREFIX_N]=""
-        PREFIX_N="$(expr $PREFIX_N - 1)"
+    if [ $LAST_OFFSET -eq 1 ]; then
+        sleep 1s
     fi
 done
-
-echo "$FINAL_FILE"
